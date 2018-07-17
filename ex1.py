a43084fec3e4aac775b9a6297fc9de5f10453290
aozpay <alicanozpay@outlook.com>
2018-07-17 17:33:28 +0300

    Üc sayinin carpimi
    Kullanıcadan alınan üç sayının çarpımını format metodu ile yazdirmak.

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 17 17:24:21 2018
+
+@author: alican
+"""
+
+sayi1 = int(input("Birinci sayiyi giriniz."))
+sayi2 = int(input("ikinci sayiyi giriniz."))
+sayi3 = int(input("Ücüncü sayiyi giriniz."))
+print("Üc sayinin carpimi = {}".format(sayi1*sayi2*sayi3 ))
